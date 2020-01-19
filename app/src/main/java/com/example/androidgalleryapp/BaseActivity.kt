cd37c0d6e9fd1299fa52c5292a9e891be718f0fb
natume-maya <hagitaevo4@yahoo.co.jp>
2020-01-19 16:27:26 +0900

    BaseActivity作成

---
+++ app/src/main/java/com/example/androidgalleryapp/BaseActivity.kt
@@ -0,0 +1,42 @@
+package com.example.androidgalleryapp
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity() {
+
+    fun startFragmentForResult(fragment: BaseFragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+    }
+
+    fun checkPermission(permission: String, requestCode: Int) {
+        val list = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        val result = IntArray(PackageManager.PERMISSION_GRANTED)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+                onRequestPermissionsResult(requestCode, list, result)
+            } else {
+                requestPermissions(list, requestCode)
+            }
+        } else {
+            onRequestPermissionsResult(requestCode, list, result)
+        }
+    }
+
+    fun onFragmentResult(requestCode: Int, resultCode: Int, intent: Intent) {
+    }
+
+    fun showProgress() {
+    }
+
+    fun dismissProgress() {
+    }
+
+    fun showError() {
+    }
+}
